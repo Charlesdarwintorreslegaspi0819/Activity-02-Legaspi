c20e32216c409490dfa6ce319fc0cc571a772a38
Charlesdarwin legaspi <107029626+Charlesdarwintorreslegaspi0819@users.noreply.github.com>
2022-06-07 16:06:30 +0800

    Add files via upload

---
+++ PokemonCalc.py
@@ -0,0 +1,12 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+charizard = 90
+ feraligatr = 95
+ 
+SyntaxError: unexpected indent
+feraligatr = 95
+modifier = round((1 * 1 * 1 * 2 * 0.85 * 1.5 * 0.25 ),2)
+damage = round(((((((2*90)/5)+2)*110*(205/188))/50)+2),2)
+tdamage = round ((damage * modifier),2)
+print('Damage Dealt: ', tdamage)
+Damage Dealt:  59.62
